f9e95a1ff7c9b4c74a0e113ac5471380cbf45523
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:51:36 -0400

    Update file_extensions.py

--- src/file_extensions.py
+++ src/file_extensions.py
@@ -2,29 +2,10 @@
 import re
 
 def file_extensions(filename):
-    empty_file_type = []
-    dict_files = {}
-    with open(filename, "r") as f:
-        for line in f:
-            if "." not in line:
-                empty_file_type.append(line[:-1])
-            else:
-                mo = re.search(r"(.*)\.(.*)$",line[:-1])
-                file_type = mo.groups()[1]
-                file_name = mo.groups()[0]
-                if file_type in dict_files:
-                    dict_files[file_type].append(f"{file_name}.{file_type}")
-                else:
-                    dict_files[file_type] = [f"{file_name}.{file_type}"]
-            
-    
-    return (empty_file_type, dict_files)
+    pass
 
 def main():
-    results = file_extensions("src/filenames.txt")
-    print(f"{len(results[0])} files with no extension")
-    for key, value in results[1].items():
-        print(f"{key} {len(value)}")
+    pass
     
     
 
